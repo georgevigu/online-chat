ab55695751920580aad54e16df1eee820365f093
George Vigu <54713418+georgevigu@users.noreply.github.com>
2024-11-05 14:58:34 +0200

    Delete src directory

--- src/main/java/dev/vigu/social_media/image/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.vigu.social_media.image;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<ImageData, Integer> {
-
-}
--- src/main/java/dev/vigu/social_media/user/Role.java
+++
@@ -1,6 +0,0 @@
-package dev.vigu.social_media.user;
-
-public enum Role {
-    USER,
-    ADMIN
-}
